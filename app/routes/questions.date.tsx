65bd2a82477b35c8c015f7564f455c836dd5afd0
Tory Briggs <tory@briggs.fun>
2024-04-17 11:30:59 -0600

    Init

--- app/routes/questions.date.tsx
+++ app/routes/questions.date.tsx
@@ -3,8 +3,6 @@ import { json } from "@remix-run/cloudflare";
 export async function loader({ context }) {
   const { env } = context.cloudflare;
 
-  console.log("Env", env);
-
   await env.demo_cache.put("date", new Date().toString(), {
     expirationTtl: 60,
   });
